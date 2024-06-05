9043872d7d867430d291e3a109175777cc756736
Ivan <ivan082403@gmail.com>
2024-06-05 10:44:11 +0800

    First Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let hours = document.querySelector("#hrs");
+let minutes = document.querySelector("#min");
+let seconds = document.querySelector("#sec");
+
+
+setInterval(()=>{
+	let currentTime = new Date();
+
+	hours.innerHTML = (currentTime.getHours() < 10 ? "0" : "")  + currentTime.getHours();
+	minutes.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "")  + currentTime.getMinutes();
+	seconds.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "")  + currentTime.getSeconds();
+
+}, 1000)
